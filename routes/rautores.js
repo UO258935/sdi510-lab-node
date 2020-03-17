6c20a049c92392b9f9955796c8bc85041d42c113
UO258935 <UO258935@uniovi.es>
2020-03-17 11:15:11 +0100

    SDI–session7-Ejercicio-complementario3

--- routes/rautores.js
+++ routes/rautores.js
@@ -44,9 +44,38 @@ module.exports = function (app, swig) {
         res.send(respuesta);
     });
 
+    app.get('/autores/filtrar/:rol', function (req, res) {
+        let autores = [{
+            "nombre": "Luis Hurlé",
+            "grupo": "Canarios Malos",
+            "rol": "Cantante"
+        }, {
+            "nombre": "Carlos Fleitas",
+            "grupo": "La Isla Rebelde",
+            "rol": "Batería"
+        }, {
+            "nombre": "Carlos Hurlé",
+            "grupo": "La Papaya",
+            "rol": "Bajista"
+        }];
+
+        let busqueda = [];
+
+        for(let a in autores){
+            if(autores[a].rol==req.params.rol){
+                busqueda.push(autores[a]);
+            }
+        }
+
+        let respuesta = swig.renderFile('views/autores.html',{
+            autores:busqueda
+        })
+
+        res.send(respuesta);
+    });
+
     app.get('/autores/*', function (req, res) {
         res.redirect('/autores');
     });
-
 
 };
