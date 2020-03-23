b51e2d40005d954467c8febe7a55628bec91d2c3
UO258935 <UO258935@uniovi.es>
2020-03-23 20:54:54 +0100

    SDI-NodeJS-8.4-Subida de ficheros y gestión de colecciones

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -1,21 +1,41 @@
-module.exports = { mongo : null, app : null,
-    init : function(app, mongo) {
+module.exports = {
+    mongo: null, app: null,
+    init: function (app, mongo) {
         this.mongo = mongo;
-        this.app = app; },
-    insertarCancion : function(cancion, funcionCallback) { this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
-        if (err) {
-            funcionCallback(null);
-        } else {
-            let collection = db.collection('canciones');
-            collection.insert(cancion, function(err, result) {
-                if (err) {
-                    funcionCallback(null);
-                } else {
-                    funcionCallback(result.ops[0]._id);
-                }
-                db.close();
-            });
-        }
-    });
+        this.app = app;
+    },
+    insertarCancion: function (cancion, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insert(cancion, function (err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    obtenerCanciones: function (criterio,funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.find(criterio).toArray(function (err, canciones) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(canciones);
+                    }
+                    db.close();
+                });
+            }
+        });
     }
 };
