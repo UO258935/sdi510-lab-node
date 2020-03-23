e160ac4b7e46a4c09c097a5cace3b5691689def9
UO258935 <UO258935@uniovi.es>
2020-03-23 21:10:19 +0100

    SDI-NodeJS-8.5,67- Registro de usuario, autenticación y uso de sesión

--- app.js
+++ app.js
@@ -7,18 +7,25 @@ app.use(fileUpload());
 
 let mongo = require('mongodb');
 
+let expressSession = require('express-session');
+app.use(expressSession({secret: 'abcdefg', resave: true, saveUninitialized: true}));
+
+let crypto = require('crypto');
+
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
 
 //Variables
 app.set('port', 8081);
-app.set('db','mongodb://admin:63988358@tiendamusica-shard-00-00-yawrt.mongodb.net:27017,tiendamusica-shard-00-01-yawrt.mongodb.net:27017,tiendamusica-shard-00-02-yawrt.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('db', 'mongodb://admin:63988358@tiendamusica-shard-00-00-yawrt.mongodb.net:27017,tiendamusica-shard-00-01-yawrt.mongodb.net:27017,tiendamusica-shard-00-02-yawrt.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
@@ -27,5 +34,5 @@ require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 //lanzar el servidor
 app.listen(app.get('port'), function () {
-        console.log("Servidor activo");
+    console.log("Servidor activo");
 });
--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -21,7 +21,7 @@ module.exports = {
             }
         });
     },
-    obtenerCanciones: function (criterio,funcionCallback) {
+    obtenerCanciones: function (criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
                 funcionCallback(null);
@@ -32,6 +32,40 @@ module.exports = {
                         funcionCallback(null);
                     } else {
                         funcionCallback(canciones);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    insertarUsuario: function (usuario, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.insert(usuario, function (err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    obtenerUsuarios: function (criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.find(criterio).toArray(function (err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
                     }
                     db.close();
                 });
